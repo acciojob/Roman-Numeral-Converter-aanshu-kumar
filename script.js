e4d1f1fb7288fa60e8cd013e0c490e8092b2a5df
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-26 16:52:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    // Define a mapping of Roman numerals to their integer values
+    const romanMap = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
     };
 
-  //your code here
+    let str = '';
+    for (const key in romanMap) {
+        while (num >= romanMap[key]) {
+            console.log(`Adding ${key} and Subtracting ${romanMap[key]} from ${num}`);
+            num -= romanMap[key];
+            str += key;
+        }
+    }
 
+    return str;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
 module.exports = convertToRoman
